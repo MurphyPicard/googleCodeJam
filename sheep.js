29219a6e96a0cdbe8e66216e239054d7c53e9667
murphypicard <ayaghsizian@gmail.com>
2017-03-29 16:02:05 -0400

    added in stuff to try running input file

--- sheep.js
+++ sheep.js
@@ -1,17 +1,36 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+// Reads complete line from STDIN
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
 
 
-
-function counting(n){
+function main(n){
 
   if (n===0) console.log('INSOMNIA');
 
   var seenTheDigitsArray = [],
-      nSplit = n.toString().split(""),
+      nSplit = (""+n).split(""),
       sheep = 0;
 
   while(seenTheDigitsArray.length < 10){
       sheep ++;
-      nSplit = (n * sheep).toString().split("");
+      nSplit = (""+(n * sheep)).split("");
       for (var i = 0; i < nSplit.length; i++){
           if($.inArray(nSplit[i], seenTheDigitsArray) === -1){
               seenTheDigitsArray.push(nSplit[i]);
